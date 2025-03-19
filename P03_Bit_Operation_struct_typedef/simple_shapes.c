eef6b442bd77be83e16e1f0071894dca8544ee44
Mario Arana <marioknaus22@gmail.com>
2025-03-19 13:32:36 +0100

    Reformat simple_shapes.c

--- P03_Bit_Operation_struct_typedef/simple_shapes.c
+++ P03_Bit_Operation_struct_typedef/simple_shapes.c
@@ -1,38 +1,39 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define RED   "\x1B[31m"
-#define GRN   "\x1B[32m"
-#define YEL   "\x1B[33m"
-#define WHT   "\x1B[37m"
+#define RED "\x1B[31m"
+#define GRN "\x1B[32m"
+#define YEL "\x1B[33m"
+#define WHT "\x1B[37m"
 #define RESET "\x1B[0m"
 
-/* 
+/*
 Students: Create a new type "Graphic" that can store:
 - shape (OVAL, RECTANGLE) -> create a new enum for this type
 - size
 - color
 */
 
-
 void paint(Graphic graphic) {
     double radius = graphic.size / 2.0;
-    int i,j;
+    int    i, j;
 
-    for (i = 0; i <= 2 * radius; i++){
-        for (j = 0; j <= 2 * radius; j++){
-            switch(graphic.shape) {
-                case RECTANGLE: printf("%s*" RESET, graphic.color); break;
+    for (i = 0; i <= 2 * radius; i++) {
+        for (j = 0; j <= 2 * radius; j++) {
+            switch (graphic.shape) {
+                case RECTANGLE:
+                    printf("%s*" RESET, graphic.color);
+                    break;
                 case OVAL: {
-                    double distance = sqrt((i - radius) * (i - radius) + (j - radius) * (j - radius));
+                    double distance = sqrt((i - radius) * (i - radius) +
+                                           (j - radius) * (j - radius));
                     if (distance > radius - 0.5 && distance < radius + 0.5) {
                         printf("%s*" RESET, graphic.color);
-                    } else { 
+                    } else {
                         printf(" ");
                     }
                 } break;
-
             }
         }
         printf("\n");
@@ -40,7 +41,7 @@ void paint(Graphic graphic) {
 }
 
 int main() {
-    int input;
+    int     input;
     Graphic graphic;
 
     do {
@@ -52,15 +53,17 @@ int main() {
         scanf("%u", &input);
         // Students: store the input in graphic
 
-        printf("Geben Sie die gewünschte Farb an [RED=0 | GREEN=1 | YELLOW=2]:");
+        printf(
+            "Geben Sie die gewünschte Farb an [RED=0 | GREEN=1 | YELLOW=2]:");
         scanf("%d", &input);
         // Students: store the input in graphic
-        
+
         paint(graphic);
-        
-        while(getchar() != '\n'); // empty buffer
+
+        while (getchar() != '\n')
+            ; // empty buffer
         printf("\nMöchten sie weiter machen oder abbrechen? [(n)ext|(q)uit] ");
-    } while(getchar() == 'n');
+    } while (getchar() == 'n');
 
     printf("Byebye..\n");
     return EXIT_SUCCESS;
