db162b7a3a5bee2b6b656db45d4e0c71df6a0b67
Mario Arana <marioknaus22@gmail.com>
2025-03-19 13:17:19 +0100

    Solve binary calculator but with quirks

--- P03_Bit_Operation_struct_typedef/bin_calculator.c
+++ P03_Bit_Operation_struct_typedef/bin_calculator.c
@@ -1,13 +1,19 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define OPERAND_BUFFER_SIZE 10
 
 typedef struct {
-    /* 
+    /*
     Students: The Expression struct should hold the two operands and
     the operation (use a char for the operation)
     */
+
+    // NOTE: DONE
+
+    int  first;
+    int  second;
+    char operator;
 } Expression;
 
 int bits_per_int() {
@@ -16,22 +22,35 @@ int bits_per_int() {
 
 unsigned int parse_operand(char operand_str[]) {
     unsigned int operand;
+
     if (operand_str[0] == '0' && operand_str[1] == 'x') {
+        // sscanf reads from a string buffer instead of stdin
         sscanf(&operand_str[2], "%x", &operand);
     } else if (operand_str[0] == '0') {
         sscanf(&operand_str[1], "%o", &operand);
     } else {
         sscanf(operand_str, "%u", &operand);
     }
+
     return operand;
 }
 
 void print_binary(unsigned int value) {
     // Students: Print a single number as a binary string
+
+    for (int i = bits_per_int() - 1; i >= 0; i--) {
+        printf("%d", (value >> i) & 1);
+
+        if (i % 8 == 0) {
+            printf("'");
+        }
+    }
+
+    printf("\n");
 }
 
 void print_bit_operation_bin(Expression expression, unsigned int result) {
-    /* 
+    /*
     Students: Print the entire operation in bin including the result
 
     Bin:
@@ -40,31 +59,63 @@ void print_bit_operation_bin(Expression expression, unsigned int result) {
     -----------------------------------
     00000000'00000000'00000000'00000011
     */
+
+    printf("Bin:\n");
+    print_binary(expression.first);
+    printf("%c\n", expression.operator);
+    print_binary(expression.second);
+    printf("-----------------------------------\n");
+    print_binary(result);
+    printf("\n");
 }
 
 void print_bit_operation_hex(Expression expression, unsigned int result) {
-    /* 
+    /*
     Students: Print the entire operation in hex including the result
 
     Hex:
     0x0c ^ 0x0f = 0x03
     */
+
+    printf("Hex:\n");
+    printf("0x%02x %c 0x%02x = 0x%02x\n", expression.first, expression.operator,
+           expression.second, result);
+
+    printf("\n");
 }
 
 void print_bit_operation_dec(Expression expression, unsigned int result) {
-    /* 
+    /*
     Students: Print the entire operation in hex including the result
 
     Dec:
     12 ^ 15 = 3
     */
+
+    printf("Dec:\n");
+    printf("%d %c %d = %d\n", expression.first, expression.operator,
+           expression.second, result);
+
+    printf("\n");
 }
 
 unsigned int bit_operation(Expression expression) {
     // Students: Do the actual bit operation and return the result
+    // NOTE: Done
+
+    switch (expression.operator) {
+        case '&':
+            return expression.first & expression.second;
+        case '|':
+            return expression.first | expression.second;
+        case '^':
+            return expression.first ^ expression.second;
+        default:
+            return 0;
+    }
 }
 
-int main(){
+int main() {
     char operand1_str[10];
     char operand2_str[10];
     char operation;
@@ -75,22 +126,26 @@ int main(){
         printf("Geben sie die Bit-Operation ein:\n");
 
         scanf("%s %c %s", operand1_str, &operation, operand2_str);
-        
+
         operand1 = parse_operand(operand1_str);
         operand2 = parse_operand(operand2_str);
 
-        Expression expression = ... ;   // Students: Create an expression
+        // Students: Create an expression
+        Expression expression = {operand1, operand2, operation};
 
         unsigned int result = bit_operation(expression);
         print_bit_operation_bin(expression, result);
         print_bit_operation_hex(expression, result);
         print_bit_operation_dec(expression, result);
 
-        while(getchar() != '\n');
+        // Clear the input buffer
+        while (getchar() != '\n')
+            ;
+
         printf("\nMöchten sie weiter machen oder abbrechen? [(n)ext|(q)uit] ");
 
-    } while(getchar() == 'n');
+    } while (getchar() == 'n');
 
-    printf("Byebye..\n");
+    printf("Bye..\n");
     return EXIT_SUCCESS;
 }
